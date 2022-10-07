c1c1ec86be11436d7d9207bccceff7a237116dd0
warshawskyj <112487742+warshawskyj@users.noreply.github.com>
2022-10-07 16:02:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 def count_word(phrase):
-    return len(phrase.split(' '))
+    '''fonction qui prend une phrase et return le nombre de mots'''
+    return len(phrase.split(' ')) #diviser la phrase en mots separees par des espaces, et ensuite compter le nombre de mots 
 
-
-while (phrase := input('Entrez une phrase: ')) != 'q':
-    print(f'La phrase contient {count_word(phrase)} mots')
+quit = False #d'abord, l'utilisateur ne veut pas quitter
+while not quit: #repeter jusqu'a l'utilisateur veut quitter
+    phrase = input("Entrez une phrase: ")
+    if phrase == 'q': #utilisateur veut quitter
+        quit = True
+    else:
+        print(f'La phrase contient {count_word(phrase)} mots') #print le nombre de mots 
 
 
